fd6d388dbef0b8fbb0ccd1afae96307f985fb77d
Jesus Alcaraz <gsus.alcaraz@gmail.com>
2023-07-10 08:59:38 +0200

    Obtiene respuesta del API

--- app/src/main/java/com/gsusalcaraz/ejercicios_aristidevs/exercises/exercises/superheroapp/SuperHeroListActivity.kt
+++ app/src/main/java/com/gsusalcaraz/ejercicios_aristidevs/exercises/exercises/superheroapp/SuperHeroListActivity.kt
@@ -1,19 +1,27 @@
 package com.gsusalcaraz.ejercicios_aristidevs.exercises.exercises.superheroapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import com.gsusalcaraz.ejercicios_aristidevs.databinding.ActivitySuperHeroListBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SuperHeroListActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySuperHeroListBinding
+    private lateinit var retrofit: Retrofit
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySuperHeroListBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        retrofit = getRetrofit()
         initUI()
     }
 
@@ -30,7 +38,23 @@ class SuperHeroListActivity : AppCompatActivity() {
     }
 
     private fun searchByName(query: String) {
+        // Lanzar rutina en hilo secundario
+        CoroutineScope(Dispatchers.IO).launch {
+            val myResponse: Response<SuperHeroDataResponse> = retrofit.create(ApiService::class.java).getSuperheroes(query)
+            if(myResponse.isSuccessful){
+                Log.i("gsus", "Funciona :)")
+            }else{
+                Log.i("gsus", "No funciona :(")
+            }
+        }
+    }
 
+    private fun getRetrofit(): Retrofit {
+        return Retrofit
+            .Builder()
+            .baseUrl("https://superheroapi.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
     }
 }
 
